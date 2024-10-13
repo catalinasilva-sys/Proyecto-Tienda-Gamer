e1f8d61618f9fbf5cc01496116fb84261c0dc47e
JoseRamirezV <jr.ramirez.varon@gmail.com>
2024-10-13 15:12:05 -0500

    codigo base

---
+++ js/navbar.js
@@ -0,0 +1,19 @@
+// // navbar.js
+// document.addEventListener("DOMContentLoaded", function() {
+//     fetch('../pages/navbar.html').then(response => response.text()).then(data => {
+//         document.getElementById('navbar-container').innerHTML = data;
+//     });
+// });
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Detectar la ubicación actual del archivo
+    let filePath = window.location.pathname;
+    
+    // Verificar si estás en index.html o en un archivo dentro de pages/
+    let pathToNavbar = (filePath.includes('/pages/')) ? './navbar.html' : './pages/navbar.html';
+    
+    // Cargar el contenido del navbar
+    fetch(pathToNavbar).then(response => response.text()).then(data => {
+        document.getElementById('navbar-container').innerHTML = data;
+    });
+});
